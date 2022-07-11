796e4a618de52b79956c2bcbf3a4267ab0d8e0be
unknowida <csa.devomax@gmail.com>
2022-07-11 23:18:43 +0200

    click and show pokemon

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg'
-import './App.css'
-import 'PokemonList'  from './PokemonList'
+import logo from './logo.svg';
+import './App.css';
+import { PokemonList} from './PokemonList';
 
 function App() {
   return (
--- src/PokemonList.jsx
+++ src/PokemonList.jsx
@@ -1,21 +1,50 @@
+import { click } from '@testing-library/user-event/dist/click'
 import React from 'react'
 
 export function PokemonList() {
-
   const [pokemons, setPokemons] = React.useState([])
 
   const onClick = async () => {
     const response = await fetch('https://pokeapi.co/api/v2/pokemon/')
     const pokemonsJSON = await response.json()
 
-    setPokemons(pokemonsJSON.results.map(pokemon => pokemon.name))
+    // sort by name
+    pokemonsJSON.results.sort(function (a, b) {
+      const nameA = a.name.toUpperCase() // ignore upper and lowercase
+      const nameB = b.name.toUpperCase() // ignore upper and lowercase
+      if (nameA < nameB) {
+        return -1
+      }
+      if (nameA > nameB) {
+        return 1
+      }
+
+      // names must be equal
+      return 0
+    })
+
+    // effacer bulbasaur
+    const filterPokemons = pokemonsJSON.results.filter(function (p) {
+      return p.name != 'bulbasaur'
+    })
+
+    setPokemons(
+      filterPokemons.map(function (pokemon) {
+        return pokemon.name
+      }),
+    )
     console.log(pokemons)
   }
 
-  return <div>
-    <button onClick={onClick}>Rechercher Pokemons</button>
-    <ul>
-        {pokemons.map(pokemon => (<li>(pokemon)</li>))}
-    </ul>
-  </div>
+  //a chaque clique, on récupère les pokemons
+  return (
+    <div>
+      <button onClick={onClick}>Rechercher Pokemons</button>
+      <ul>
+        {pokemons.map((pokemon) => (
+          <li>{pokemon}</li>
+        ))}
+      </ul>
+    </div>
+  )
 }
